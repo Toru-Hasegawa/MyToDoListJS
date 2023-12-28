9580ed010dd5eab9a8f7921768d117832dc0184a
ToruHasegawa <toruhasegawa0911@gmail.com>
2023-12-29 00:07:39 +0900

    20231229_1コミット

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config({
+        path: `${__dirname}/.env`
+    });
+}
+
 const express = require("express");
 const path = require("path");
 const middleware = require("./middleware/middleware");
 const errorHandlingMiddleware = require("./middleware/error-handling");
+const PORT = process.env.PORT || 9000;
 
 // Controllers
 const UsersController = require("./controllers/users-controller");
 const ToDosController = require('./controllers/todos-controller');
 
 const app = express();
-const clientRoot = path.resolve(__dirname, "..", "/client");
+const clientRoot = path.resolve(__dirname, "..", "/client/dist/packet-app-service");
 
 middleware(app, clientRoot);
 
@@ -16,11 +23,11 @@ app.use("/api/users", UsersController);
 app.use("/api/todos", ToDosController);
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.sendFile(path.join(clientRoot, "index.html"));
 });
 
 errorHandlingMiddleware(app);
 
-app.listen(9000, () => {
-    console.log("Server running on port 9000");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
